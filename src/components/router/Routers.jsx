08bc02ddf36ccc28fd4cf4a28ab41f3c31b612f8
varsha <tadakamadlavarsha@gmail.com>
2025-12-28 23:58:14 +0530

    Add Register page with form handling and navigation

--- src/components/router/Routers.jsx
+++ src/components/router/Routers.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "../../pages/Home"
 import Tours from "../../pages/Tours"
 import Login from "../../pages/Login"
+import Register from "../../pages/Register"
 
 export const Routers=()=>{
     return(
@@ -12,6 +13,7 @@ export const Routers=()=>{
              <Route path="/home" element={<Home/>}/>
              <Route path="/tours" element={<Tours/>}/>
              <Route path="/login" element={<Login/>}/>
+             <Route path="/register" element={<Register/>}/>
         </Routes>
         
         </>
